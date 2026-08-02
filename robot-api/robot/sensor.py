19b8d6225f7d712c150258fa471707601dd65c43
Shyam Kishan <99098881+Shyam-Kishan@users.noreply.github.com>
2026-08-02 16:18:06 -0700

    more debugging changes

--- robot-api/robot/sensor.py
+++ robot-api/robot/sensor.py
@@ -6,7 +6,6 @@ ser = serial.Serial('/dev/cu.usbserial-130', 9600, timeout=1)
 def get_distance():
     try: 
         line = ser.readline().decode('utf-8').strip()
-        print(line)
         if line.startswith("DIST: "):
             value = line.split("DIST: ")[1].strip()
             return float(value)
@@ -14,3 +13,8 @@ def get_distance():
         pass
 
     return None
+
+while True:
+    dist = get_distance()
+    if dist is not None:
+        print(f"Distance: {dist}")
