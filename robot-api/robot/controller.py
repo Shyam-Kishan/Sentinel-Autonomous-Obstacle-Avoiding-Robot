5d14dc833d2565ae4943da8290f872c001195770
Shyam Kishan <99098881+Shyam-Kishan@users.noreply.github.com>
2026-08-02 14:52:02 -0700

    added commands for auto and manual modes

--- robot-api/robot/controller.py
+++ robot-api/robot/controller.py
@@ -3,14 +3,11 @@ import serial
 ser = serial.Serial('/dev/cu.usbserial-130', 9600)
 
 def move_robot(command):
-    valid_commands = ["forward", "backward", "left", "right", "stop"]
+    valid_commands = ["forward", "backward", "left", "right", "stop", "auto", "manual"]
 
     if command not in valid_commands:
         return "Invalid command"
 
-    # Replace this later with Arduino serial communication
-    # print(f"Robot moving: {command}")
-    # return f"Executed {command}"
-    print(f"[DEBUG] Received command: {command}")
+    # print(f"[DEBUG] Received command: {command}")
     ser.write(command.encode())
     return f"Sent {command} to robot"
